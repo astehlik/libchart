ae6db0f4d37482fb1412d4ff32430b708e96ce8d
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-10-03 09:50:01 +0200

    Do not try to upload coverage for pull requests

--- .travis/test_phpunit.sh
+++ .travis/test_phpunit.sh
@@ -7,7 +7,7 @@ echo "Running unit tests";
 composer install --prefer-dist
 
 
-if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" ]]; then
+if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]; then
     curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
     chmod +x ./cc-test-reporter
     ./cc-test-reporter before-build
